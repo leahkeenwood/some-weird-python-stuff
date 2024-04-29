447000c089069babd603e7cb42eabafb8314e24f
Logan Vessell <logan@loganvessell.com>
2024-04-29 15:59:59 -0500

    Bug: This assignment hates better code. Fixing gradeQuestions.

--- challenge02/main.py
+++ challenge02/main.py
@@ -24,13 +24,10 @@ def answerQuestion(question):
 
 # gradeQuestion takes the answerQuestion's answer and checks if the answer is correct.
 # The function then provides points to the user.
-def gradeQuestion(question, userAnswer):
-   # Setting points as a global value.
-   global points
-
-   # These vaiables will take the correct answer and user answer converting it to lowercase.
+def gradeQuestion(question, userAnswer, points):
+   # These variables will take the correct answer and user answer converting it to lowercase.
    # If the question is Multiple Choice, it splits the answer accordingly.
-   correctAnswerLowercase = quiz[question].getCorrectAnswer().lower().split(". ")
+   correctAnswerLowercase = quiz[question].getCorrectAnswer().lower()
    userAnswerLowercase = userAnswer.lower()
 
    # With the new variables, we can check if the user answer applies to the correct answer.
@@ -38,9 +35,7 @@ def gradeQuestion(question, userAnswer):
    if userAnswerLowercase not in correctAnswerLowercase:
       print(f"Incorrect... The answer is {quiz[question].getCorrectAnswer()}.\n")
    else:
-      # This function can tally up the points, offering the points you received, and how many total points you have overall.
-      points += quiz[question].getPoints()
-      print(f"\nYou earned {quiz[question].getPoints()} points! \nYou now have {points} points.\n")
+      print(f"\nYou earned {points()} points!")
 
 # This is the actual script itself.
 if __name__ == "__main__":
@@ -49,11 +44,11 @@ if __name__ == "__main__":
 
    # We set up the quiz into a list, using each item as a question.
    quiz = [
-      MultipleChoice("Which animal is the Drexel mascot?", "a. Dragon", 10, ["a. Dragon", "b. Tiger", "c. Bulldog"]),
+      MultipleChoice("Which animal is the Drexel mascot?",["a. Dragon", "b. Tiger", "c. Bulldog"], "a", 10),
       FillInTheBlank("The name of Drexel University's founder is ____ J. Drexel.", "Anthony", 10),
    ]
 
    # In doing a list for the questions, we can do a for loop to go through each question, making our code cleaner overall.
    for index in range(len(quiz)):
       answerQuestion(index)
-      gradeQuestion(index, answer)
+      gradeQuestion(index, answer, quiz[index].getPoints)
